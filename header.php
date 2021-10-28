1f4abf8455e4b0145244acefa6ccca979e3a0ce7
noman122official <67466365+noman122official@users.noreply.github.com>
2021-10-28 12:27:18 +0530

    project init

---
+++ header.php
@@ -0,0 +1,41 @@
+<link rel="stylesheet" type="text/css" href="css/header.css">
+        <div class="header_main">
+		<div class="navBar">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="index.php#aboutus">About Us</a></li>
+                <li><a href="index.php#contactus">Contact Us</a></li>
+				<li><a href="staff_login.php">Staff Login</a></li>
+				<li><a href="staff_login.php">Customer Login</a></li>
+				<li><a href="staff_login.php">Customer Register</a></li>
+			
+			</ul>
+		</div>
+	 <a href="index.php"><div class="logo-name">
+			<div class="logo">
+             <img class="logo_img" src="img/i2it_logo.jpg" alt="logo">
+			</div>
+			
+			<div class="name">
+				<h5> I2IT Student Banking Portal</h5></a><br>
+				<h6>Hinjewadi</h6>
+			</div>
+        </div>
+            
+            
+            <div class="dif_banking">
+			<div class="open_account">
+				<a href="customer_reg_form.php">Open Account</a>
+			</div>
+			<div class="fund_transfer">
+				<a href="fund_transfer.php"> Fund Transfer</a>
+			</div>
+			<div class="debit_card">
+				<a href="#"> Apply Debit Card</a>
+			</div>
+	
+		</div>
+            
+           
+			
+	</div>
---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+<title>Online Banking System</title>
+<head>
+   <link rel="stylesheet" type="text/css" href="css/index.css">
+   <link rel="shortcut icon" href="img/chase.jpg">
+    </head>
+ 
+<body>
+<?php include'header.php'?>
+<div class="index_container">
+    <div class="slider">
+    <div class="slideimg">
+    <img src="img/onbnk.jpg">
+        <img src="img/onbnk.jpg">
+        <img src="img/onbnk.jpg">
+        <img src="img/onbnk.jpg">
+        <img src="img/onbnk.jpg">
+        </div>
+    </div><br>
+   
+   
+
+    
+<!-- <div class="news_events">
+    <h4>Hello</h4><br>
+        <ul>
+            <p>Open an Account here
+ </p><br>
+<p>And finally, proceed for Internet Banking Registration to create your internet banking account.
+
+</p>
+            <br>
+        </ul>
+    </div> -->
+    
+
+   
+
+        <div id="aboutus" class="about"><span>About Us</span><br><br>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+</p></div>
+    
+    
+
+
+    </div>
+    
+<?php include 'footer.php';?>
+</body>
+
+</html>
